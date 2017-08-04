743059e32e0a609fd5a287d6eaa361f0e788c507
Alissara <alissara.r@gmail.com>
2017-08-04 08:14:55 -0700

    Add Bench controller index & create

--- app/controllers/api/benches_controller.rb
+++ app/controllers/api/benches_controller.rb
@@ -1,9 +1,24 @@
 class Api::BenchesController < ApplicationController
 
   def index
+    @benches = Bench.all
+    render :index
   end
 
-  def create 
+  def create
+    @bench = Bench.create!(bench_params)
+    render :show
+  end
+
+  private
+  def bench_params
+    params.require(:bench).permit(
+      :lat,
+      :lng,
+      :description,
+      :seating,
+      :picture_url
+    )
   end
 
 
